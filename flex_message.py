79fc04b65aa955e9bcdca7d3b8262761f3ac50a8
康恆 徐 <wa0125678665@>
2023-02-15 06:21:32 +0800

    make it better

--- flex_message.py
+++
@@ -1,7 +0,0 @@
-line_bot_api.reply_message(
-    event.reply_token,
-    FlexSendMessage(
-        alt_text = '全台水庫資訊',
-        contents = json.load(open('reservoir.json', 'r', encoding='utf-8'))
-    )
-)
--- requirements(1).py
+++
@@ -1,4 +0,0 @@
-#flask
-#beautifulsoup4
-#line-bot-sdk==1.18.0
-#requests
